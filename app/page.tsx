9587b983b67c1318ef5a81497c1f42c15f53a357
Boluwatife Ajibola <ajibolss11@gmail.com>
2024-05-22 19:39:57 -0400

    add home page ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,36 @@
-import { Input } from "@mantine/core";
+import { Input, Stack, Center, Title, Button, Group } from "@mantine/core";
 
 export default function Home() {
+  const xray = {
+    root: {
+      outline: "2px solid blue",
+    },
+  };
+
   return (
-    <main>
-      <h1>Welcome to todo-fy</h1>
-      <Input size="md" placeholder="Email" />
-    </main>
+    <Stack h={"100vh"} styles={xray} bg={"green.8"}>
+      <Title ml={"xl"} c={"white"} size={"2.7rem"} order={1}>
+        TODO-fy
+      </Title>
+
+      <Stack align="center" justify="center" h={"80%"}>
+        <Title c={"white"} size={"6rem"}>
+          Your all in one productivity tool
+        </Title>
+
+        <Title size={"2rem"} c={"white"}>
+          Speed up your productivity with TODO-fy! Productivity as never been
+          easier
+        </Title>
+        <Group w={"22rem"} justify="space-between" mt={"xl"}>
+          <Button color="white" variant="light" size="xl">
+            Get Started
+          </Button>
+          <Button color="white" variant="outline" size="xl">
+            Login
+          </Button>
+        </Group>
+      </Stack>
+    </Stack>
   );
 }
