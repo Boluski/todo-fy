7ec38573e4a7aae1df574e797ee023483846ecf5
Boluwatife Ajibola <ajibolss11@gmail.com>
2024-05-25 13:35:30 -0400

    add sign up fonctionality to the page

--- src/app/getStarted/page.tsx
+++ src/app/getStarted/page.tsx
@@ -2,6 +2,9 @@
 
 import { useState } from "react";
 import { useRouter } from "next/navigation";
+import { signUp, confirmSignUp } from "aws-amplify/auth";
+import config from "../../amplifyconfiguration.json";
+import { Amplify } from "aws-amplify";
 import {
   Stack,
   Stepper,
@@ -15,7 +18,9 @@ import {
   ColorPicker,
   ColorSwatch,
   DEFAULT_THEME,
+  PinInput,
 } from "@mantine/core";
+
 export default function GetStarted() {
   const xray = {
     root: {
@@ -26,9 +31,11 @@ export default function GetStarted() {
   const router = useRouter();
   const [active, setActive] = useState(0);
   const [theme, setTheme] = useState(DEFAULT_THEME.colors.green[8]);
-  const nextStep = () => {
+  const nextSlide = () => {
     setActive((current) => (current < 3 ? current + 1 : current));
   };
+
+  Amplify.configure(config);
 
   const [userName, setUserName] = useState("");
   const [displayName, setDisplayName] = useState("");
@@ -36,13 +43,55 @@ export default function GetStarted() {
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
 
-  const handleSignUp = () => {
+  const [verificationCode, setVerificationCode] = useState("");
+
+  const handleSignUp = async () => {
     console.log(userName);
     console.log(displayName);
     console.log(email);
     console.log(password);
     console.log(confirmPassword);
-    nextStep();
+    if (
+      userName == "" ||
+      displayName == "" ||
+      email == "" ||
+      password == "" ||
+      confirmPassword == ""
+    ) {
+      console.log("Empty fields");
+    } else {
+      try {
+        if (password == confirmPassword) {
+          await signUp({
+            username: userName,
+            password: password,
+            options: {
+              userAttributes: {
+                email: email,
+              },
+            },
+          });
+          nextSlide();
+        } else {
+          console.log("Password don't match");
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  };
+
+  const handleVerification = async () => {
+    console.log(verificationCode);
+    try {
+      await confirmSignUp({
+        username: userName,
+        confirmationCode: verificationCode,
+      });
+      nextSlide();
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   return (
@@ -57,7 +106,11 @@ export default function GetStarted() {
             size="xl"
             p={"xl"}
           >
-            <StepperStep label="First step" description="Create an account">
+            <StepperStep
+              allowStepSelect={false}
+              label="First step"
+              description="Create an account"
+            >
               <Stack>
                 <Stack gap={0}>
                   <Text size="1.25rem">Username:</Text>
@@ -88,6 +141,7 @@ export default function GetStarted() {
                 <Stack gap={0}>
                   <Text size="1.25rem">Email Address:</Text>
                   <TextInput
+                    inputMode="email"
                     value={email}
                     onChange={(e) => {
                       setEmail(e.currentTarget.value);
@@ -133,6 +187,7 @@ export default function GetStarted() {
                   >
                     Login
                   </Button>
+
                   <Button color="green.8" size="md" onClick={handleSignUp}>
                     Get Started
                   </Button>
@@ -140,7 +195,11 @@ export default function GetStarted() {
               </Stack>
             </StepperStep>
 
-            <StepperStep label="Second step" description="Verify email">
+            <StepperStep
+              allowStepSelect={false}
+              label="Second step"
+              description="Verify email"
+            >
               <Stack
                 h={"30rem"}
                 // styles={xray}
@@ -157,16 +216,36 @@ export default function GetStarted() {
                   w={"40rem"}
                   style={{ textAlign: "center" }}
                 >
-                  We have sent you an email to verify that you are human. Check
-                  your spam folder, if it's not in your inbox.
+                  We have sent you an email with a code to verify that you are
+                  human. Check your spam folder, if it's not in your inbox.
                 </Title>
-                <Button mt={24} color="green.8" size="lg" onClick={nextStep}>
+                <PinInput
+                  length={6}
+                  mt={24}
+                  size="xl"
+                  inputMode="numeric"
+                  placeholder=""
+                  value={verificationCode}
+                  onComplete={(e) => {
+                    setVerificationCode(e);
+                  }}
+                />
+                <Button
+                  mt={24}
+                  color="green.8"
+                  size="lg"
+                  onClick={handleVerification}
+                >
                   Email Verified
                 </Button>
               </Stack>
             </StepperStep>
 
-            <StepperStep label="Final step" description="Your project">
+            <StepperStep
+              allowStepSelect={false}
+              label="Final step"
+              description="Your project"
+            >
               <Stack justify="center" h={"30rem"}>
                 <Stack gap={0}>
                   <Text size="1.25rem">Project Name:</Text>
